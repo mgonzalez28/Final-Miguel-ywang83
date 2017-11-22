19b73dae80fa28beb41b7aaf51c9359f23d61165
rogerld <ywang83@luc.edu>
2017-11-21 20:52:43 -0600

    update

--- src/main/java/edu/luc/cs271/myhashmap/Main.java
+++ src/main/java/edu/luc/cs271/myhashmap/Main.java
@@ -10,7 +10,7 @@ public class Main {
 
     final Scanner input = new Scanner(System.in).useDelimiter("[^\\p{Alnum}]+");
     //    Path path =
-    // Paths.get(System.getProperty("user.dir")).resolve("C:\\Users\\di\\IdeaProjects\\lab11-lab9-ywang83\\src\\main\\java\\edu\\luc\\cs271\\myhashmap\\lesmisrables01unkngoog_djvu.txt");
+    // Paths.get(System.getProperty("user.dir")).resolve("C:\\Users\\IdeaProjects\\src\\main\\java\\edu\\luc\\cs271\\myhashmap\\lesmisrables01unkngoog_djvu.txt");
     //    final Scanner input = new Scanner(new FileReader(path.toFile()));
 
     // TODO measure the performance for MyHashMap, HashMap, and TreeMap several times each!
