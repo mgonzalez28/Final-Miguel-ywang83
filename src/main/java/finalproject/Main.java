87a57d8fbf96ad42a3795a4aabc5d5b1cebfa04a
ywang83 <ywang83@luc.edu>
2017-11-27 09:04:12 -0600

    creat game window

--- src/main/java/finalproject/Main.java
+++ src/main/java/finalproject/Main.java
@@ -1,9 +1,35 @@
 package finalproject;
 
-import java.util.*;
+import javax.swing.*;
+import java.awt.event.*;
 
-public class Main {
-  public static void main(final String[] args) {
-
+public class Main implements ActionListener {
+  public static void main( String[] args) {
+    Main p1 = new Main();
   }
+
+    JFrame frame=new JFrame("Hungry Snake");
+    JButton button;
+
+    Main()
+    {
+      button=new JButton("Start Game");
+      frame.setSize(400,400);
+      frame.setVisible(true);
+      frame.setLayout(null);
+      frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+
+      frame.getContentPane().add(button);
+      button.setBounds(120,120,100,30);
+
+      button.addActionListener(this);
+    }
+
+  public void actionPerformed(ActionEvent e)
+  {
+    frame.dispose();
+    gamePlay p2=new gamePlay();
+  }
+
+
 }
--- src/main/java/finalproject/gamePlay.java
+++ src/main/java/finalproject/gamePlay.java
@@ -1,6 +1,16 @@
 package finalproject;
 
 
+import javax.swing.*;
+
 public class gamePlay {
+    JFrame frame=new JFrame("Level 1");
+
+    gamePlay()
+    {
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.setSize(700,600);
+    }
 
 }
