11147e2577f72065f6210419a75f3f9727192e71
rogerld <ywang83@luc.edu>
2017-11-27 16:16:52 -0600

    update

--- src/main/java/finalproject/gameControl.java
+++ src/main/java/finalproject/gameControl.java
@@ -1,6 +1,32 @@
 package finalproject;
 
 
-public class gameControl {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferStrategy;
+import java.util.LinkedList;
+import java.util.Random;
 
+
+public class gameControl extends Panel {
+
+    private LinkedList<Point> snake;
+    private Point apple;
+
+    int appleX = 200;
+    int appleY = 200;
+
+    int snakeX = 300;
+
+    int pointWidth = 20;
+    int pointHeight = 20;
+
+    public void paint(Graphics graphic){
+
+        // create the apple
+        graphic.setColor(Color.yellow);
+        graphic.fillOval(appleX,appleY,pointWidth,pointHeight);
+    }
 }
--- src/main/java/finalproject/gamePlay.java
+++ src/main/java/finalproject/gamePlay.java
@@ -2,15 +2,21 @@ package finalproject;
 
 
 import javax.swing.*;
+import java.awt.*;
 
 public class gamePlay {
     JFrame frame=new JFrame("Level 1");
+    gameControl control = new gameControl();
+
 
     gamePlay()
     {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
-        frame.setSize(700,600);
+        frame.setSize(640,480);
+        frame.add(control);
     }
+
+
 
 }
